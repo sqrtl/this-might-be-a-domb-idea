a540243c331846c79e2b129fa5a18a49b3531960
Brasil <squirtle.brazil@gmail.com>
2022-03-14 12:31:01 -0400

    Finishing product

---
+++ main.js
@@ -0,0 +1,26 @@
+let paragraph = document.querySelector('p');
+paragraph.style.color = 'lightblue';
+
+let fontSize = document.querySelector('h1');
+fontSize.style.fontSize = '48px';
+
+paragraph.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.';
+
+let item13 = document.querySelector('#item-13');
+item13.style.opacity = '.5';
+
+let item3 = document.querySelector('#item-3');
+item3.innerText = 'I say, "Hi!"';
+
+let image = document.querySelector('img');
+image.src = 'http://www.tioxic.com/wp-content/uploads/trex_4.jpg';
+
+image.style.height = '300px';
+
+let chaos = document.querySelector('#chao-garden');
+chaos.style.height = '300px';
+
+let item16 = document.querySelector('#item-16');
+item16.className = 'item';
+
+item16.innerText = 'Won\'t be fooled again.'
